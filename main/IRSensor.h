2cb2b1f3435edbc1dfeb0cfad33eb947e677b8cc
Taran Basati <TaranBasati@cmail.carleton.ca>
2024-11-18 16:15:51 -0500

    Add files via upload

---
+++ main/IRSensor.h
@@ -0,0 +1,15 @@
+// IRSensor.h
+#ifndef IRSENSOR_H
+#define IRSENSOR_H
+
+#include <Arduino.h>
+
+// Pin definitions for IR sensors
+#define IR_SENSOR_1_PIN 9  // First IR sensor connected to digital pin 9
+#define IR_SENSOR_2_PIN 10 // Second IR sensor connected to digital pin 10
+
+// Function prototypes
+bool detect_IR_1();
+bool detect_IR_2();
+
+#endif // IRSENSOR_H
---
+++ main/LineDetection.h
@@ -0,0 +1,13 @@
+// LineDetection.h
+#ifndef LINEDETECTION_H
+#define LINEDETECTION_H
+
+#include <Arduino.h>
+
+// Threshold value for line detection
+#define THRESHOLD 970
+
+// Function prototype
+bool detect_line(int test);
+
+#endif // LINEDETECTION_H
